fb8b402ac9f991844cc69e0938daf466369f4b90
Elham <elham.bastani@gmail.com>
2019-11-27 20:43:57 -0800

    changed GUI, next page working

--- fin-lit/src/components/QuestionPage.js
+++ fin-lit/src/components/QuestionPage.js
@@ -1,8 +1,9 @@
 import React, {useState,useEffect}  from 'react';
 
 const QuestionPage = (props) => {
-  const [questionCode, setQuestionCode] = useState('');
+  //const [questionCode, setQuestionCode] = useState('');
   const [question, setQuestion] = useState('');  
+  const [nextCode, setNextCode] = useState('');  
   
   
   useEffect(() => {
@@ -11,7 +12,7 @@ const QuestionPage = (props) => {
   }, []);
 
 const loadQuestion = (code)=>{
-  setQuestionCode(code);
+  //setQuestionCode(code);
   console.log('load question');
   console.log(code);
   fetchData(code);
@@ -35,7 +36,8 @@ const loadQuestion = (code)=>{
     .then(
       (result) => {
         setQuestion(result.body);
-        console.log(result);
+        setNextCode(result.nextId)
+        console.log(result.nextId);
         console.log('got the info ');
     })
       .catch((error) => {
@@ -50,15 +52,23 @@ var temp = props.questionCode;
 
 
 const moveToNextPage = () =>{
-    //loadQuestion();
+  loadQuestion(nextCode);
+  console.log('got the info ');
 };
 
   return (
   <React.Fragment>
-  <div className="container">
+  <div className="row" >
+      <div className="ml-5   mt-2 ">
+        <h3><span class="badge badge-danger float-left">Finance Planner</span></h3>
+      </div>
+    </div>
 
-    <div className="row" >
-        <div className="container col-6 ">
+  <div className="container Que-Container mt-5">
+
+
+    <div className="row " >
+        <div className="container mt-5 col-6 ">
             <br/>
         <h4>{question}</h4>
         <br/>
@@ -66,13 +76,15 @@ const moveToNextPage = () =>{
     </div>
 
     <div className="row ">
-      <div className="container col-6">
-        <h4 >About this App</h4>
+      <div className="container mt-5 col-6">
+          <div className="form-group">
+            <input className="form-control" type="number" />
+          </div>
       </div>
     </div>
 
 
-    <div className="row ">
+    <div className="row mt-5 pb-2">
       <div className="col-6">
         <button  type="button" onClick={(e) => {e.preventDefault();props.history.goBack();}} className="btn float-left" >Back</button>
       </div>
